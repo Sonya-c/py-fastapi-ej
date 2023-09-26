1b98d6ec2312281b723b06843602414402422172
Sonya Castro <sonya.castro.gomez@gmail.com>
2023-09-26 11:20:38 -0500

    schemas

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import Body, FastAPI 
+from pydantic import BaseModel
 
 app = FastAPI() # Create an instance
+
+# schema: contract client-server, so the client send you the expected data
+# we're gonna use pydantic
+# schema:
+#    title: srt
+#    content: srt
+
+class Post(BaseModel):
+    title: str 
+    body: str
 
 # Path (or route) operation 
 @app.get("/") # decorator "magic to the function"
@@ -16,10 +27,14 @@ def root(): # function
 def get_post():
     return {"message": "All post"}
 
-@app.post("/posts") # post: we can send date to the server 
-def create_post(payload: dict = Body):
-    print(payload)
+@app.post("/posts") # post: we can send data to the server 
+def create_post(post:Post): # as we use a model, fastApi gonna validate this
+    print(f"title= {post.title}") # it automatically extract the data
+
+    # it the data it's invalid, fastAPI send an error msg
+    # it we recive an number, we can convert to string, so still working 
     return {
         "message": "Post created sucessfull",
-        "post": payload,
+        "post": post,
     }
+
